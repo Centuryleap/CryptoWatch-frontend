0b5d966fb4ebf3efa3b4464b8c79e5e9ff81e62a
Ganiyu Idris <Golamide27@gmail.com>
2022-07-15 14:22:31 +0100

    Added some utitlities

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tutorial/>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  middleware({ app, redirect }) {
+    redirect(app.$dashboard("home"));
+  }
 }
 </script>
