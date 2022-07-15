3904e75853e59e0c311007c129ff8ce76dd86289
Ganiyu Idris <Golamide27@gmail.com>
2022-07-15 13:21:46 +0100

    Added plugins and intitialize taiwlind

---
+++ plugins/utilities.js
@@ -0,0 +1,12 @@
+const resolve = (base, path = '/') => `/${base}${path.startsWith('/') ? path : '/' + path}`
+
+export default (ctx, inject) => {
+  inject('user', (path) => resolve('user', path))
+  // inject('signup', (path) => resolve('user/signup', path))
+  // inject('password', (path) => resolve('user/password', path))
+  // inject('dashboard', (path) => resolve('dashboard', path))
+  // inject('profile', (path) => resolve('dashboard/profile', path))
+  // inject('settings', (path) => resolve('dashboard/settings', path))
+  // inject('loanApplication', (path) => resolve('dashboard/loans/application', path))
+  // inject('editProfile', (path) => resolve('dashboard/profile/edit', path))
+}
