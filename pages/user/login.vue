14f13ddc4a2ccc6dc6992294a5f3fde43217f531
Ganiyu Idris <Golamide27@gmail.com>
2022-07-15 16:38:30 +0100

    Created the login page

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -3,16 +3,23 @@
     title="Sign in to your account"
     description="Enter you details to sign in to your account "
   >
-    <div class="inputs w-full mt-9 lg:mt-12 space-y-5">
+    <div class="inputs w-full mt-9 lg:mt-12 space-y-4 lg:space-y-5">
       <InputField type="email" placeholder="Email address" />
       <InputField type="password" placeholder="Password" />
-      <InputField type="password" placeholder="Confirm password" />
     </div>
 
-    <ActionButton class="mt-8 lg:mt-10"> Continue </ActionButton>
-    
+    <div class="w-full between mb-3 lg:mb-3.5 text-xs mt-12 lg:mt-16">
+      <div class="remember-me text-primary-3 font-light">Remember me</div>
+
+      <span class="block">
+        <Link to="/user/login"> Forgot your pin?</Link>
+      </span>
+    </div>
+
+    <ActionButton> Continue </ActionButton>
+
     <div class="hint">
-      Already have an account? <Link to="/user/login"> Sign In</Link>
+      You don't have an account? <Link to="/user/signup"> Create one</Link>
     </div>
   </Modal>
 </template>
@@ -28,6 +35,6 @@ export default {
 
 <style lang="postcss" scoped>
 .hint {
-  @apply mt-3 lg:mt-4 text-sm font-light text-text-3;
+  @apply mt-3 lg:mt-4 text-xs lg:text-sm font-light text-text-3;
 }
 </style>
--- pages/user/signup/details.vue
+++ pages/user/signup/details.vue
@@ -3,14 +3,14 @@
     title="Create an account"
     description="Create an account to get started started"
   >
-    <div class="inputs w-full mt-9 lg:mt-12 space-y-5">
+    <div class="inputs w-full mt-9 lg:mt-12 space-y-4 lg:space-y-5">
       <InputField type="email" placeholder="Email address" />
       <InputField type="password" placeholder="Password" />
       <InputField type="password" placeholder="Confirm password" />
     </div>
 
     <ActionButton class="mt-8 lg:mt-10"> Continue </ActionButton>
-    
+
     <div class="hint">
       Already have an account? <Link to="/user/login"> Sign In</Link>
     </div>
@@ -28,6 +28,6 @@ export default {
 
 <style lang="postcss" scoped>
 .hint {
-  @apply mt-3 lg:mt-4 text-sm font-light text-text-3;
+  @apply mt-3 lg:mt-4 text-xs lg:text-sm font-light text-text-3;
 }
 </style>
