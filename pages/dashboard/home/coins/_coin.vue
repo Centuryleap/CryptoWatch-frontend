f55ca257f2f5d010a6b4832c6526b285fc93e44d
Ganiyu Idris <Golamide27@gmail.com>
2022-08-10 11:56:49 +0100

    Created the single coin card on the dynamic coins page

--- pages/dashboard/home/coins/_coin.vue
+++ pages/dashboard/home/coins/_coin.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="coin-card">
-      <div class="coin-card-container between">
+      <div class="coin-card-container">
         <div class="left between md:block">
           <div class="inner-left start">
             <Icon src="btc" />
@@ -35,16 +35,14 @@
           </div>
         </div>
 
-        <div class="right">
-          <div class="top">
+        <div class="right md:col-between">
+          <div class="top between">
             <div class="high">
               <span class="text">
                 {{ coinData.high_24h }}
               </span>
 
-              <span class="label">
-                High
-              </span>
+              <span class="label"> High </span>
             </div>
 
             <div class="low">
@@ -52,9 +50,7 @@
                 {{ coinData.low_24h }}
               </span>
 
-              <span class="label">
-                Low
-              </span>
+              <span class="label"> Low </span>
             </div>
 
             <div class="volume-coin">
@@ -72,10 +68,18 @@
                 {{ usdtVolume }}
               </span>
 
-              <span class="label">
-                Volume (USDT)
-              </span>
+              <span class="label"> Volume (USDT) </span>
             </div>
+          </div>
+
+          <div class="bottom end">
+            <button class="set-price-alert">Set price alert</button>
+
+            <button class="add-to-watchlist">
+              <Icon src="star-outline" class="inline" />
+
+              <span>Add to watchlist</span>
+            </button>
           </div>
         </div>
       </div>
@@ -140,12 +144,12 @@ export default {
 
 <style lang="postcss" scoped>
 .coin-card {
-  @apply bg-primary-1 rounded-2xl md:rounded-3xl lg:rounded-[32px] py-[34px] px-6 md:px-10 lg:px-16 xl:px-[72] md:pt-12 lg:pt-16 md:pb-[9] lg:pb-[50px];
+  @apply bg-primary-1 rounded-2xl md:rounded-3xl lg:rounded-[32px] py-[34px] px-6 md:px-10 lg:px-16 xl:px-[72] md:pt-12 lg:pt-16 md:pb-[9] lg:pb-[50px] xl:pb-14 max-w-[1080px];
 
   > .coin-card-container {
-    @apply w-full;
+    @apply w-full flex justify-between;
     > .left {
-      @apply w-full md:space-y-7 lg:space-y-10;
+      @apply w-full md:space-y-7 lg:space-y-10 md:w-1/2;
       > .inner-left {
         @apply space-x-2;
 
@@ -165,7 +169,7 @@ export default {
       > .inner-right {
         @apply md:space-y-1 lg:space-y-0;
         span.price {
-          @apply text-[#FAFAFA] font-light block md:text-2xl md:font-semibold;
+          @apply text-[#FAFAFA] font-light block md:text-2xl xl:text-[32px] xl:leading-[37px] md:font-semibold;
         }
 
         span.current-price-percentage {
@@ -175,14 +179,33 @@ export default {
     }
 
     > .right {
-      @apply hidden md:block;
+      @apply hidden md:flex w-1/2;
       > .top {
-        @apply space-x-5 lg:space-x-10;
+        @apply space-x-5 lg:space-x-10 w-full;
 
-        > .high, .low, .volume-coin, .volume-usd {
+        > .high,
+        .low,
+        .volume-coin,
+        .volume-usd {
           span.text {
-            @apply text-[#FAFAFA] text-sm
+            @apply text-[#FAFAFA] text-sm lg:text-base block;
           }
+
+          span.label {
+            @apply text-[10px] xl:text-xs text-text-3 font-light block;
+          }
+        }
+      }
+
+      > .bottom {
+        @apply space-x-4 xl:space-x-[18px] min-w-full;
+
+        > .set-price-alert {
+          @apply rounded-[12px] py-3 lg:py-[13px] px-2.5 lg:px-3 bg-primary-3 font-light text-xs lg:text-sm text-white;
+        }
+
+        > .add-to-watchlist {
+          @apply rounded-[12px] py-3 px-3 text-sm lg:text-base font-light text-[#FAFAFA] bg-[#F44B03] inline-flex items-center space-x-1;
         }
       }
     }
